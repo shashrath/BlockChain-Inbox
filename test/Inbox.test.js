2fab7433aa646465d8b42220a0831c0c97c68869
shashrath <rathoreshashank0@gmail.com>
2019-01-31 22:35:14 +0530

    Test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,6 +8,7 @@ var web3 = new Web3(provider);
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = 'Hi There';
 
 beforeEach( async () => {
     // Get a list of Free Accounts
@@ -15,7 +16,7 @@ beforeEach( async () => {
     
     // Use those account to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi There']})
+        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
         .send({from: accounts[0], gas: '1000000'});
 
     inbox.setProvider(provider);
@@ -26,7 +27,13 @@ describe('Inbox',() => {
         console.log(inbox.options.address);
         assert.ok(inbox.options.address);
     });
-    it('Initial message set', () => {
-        assert.equal(inbox.methods.message(),'Hi There');
+    it('Initial message set', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message,INITIAL_MESSAGE);
     });
+    it('SetMessage function working', async () => {
+        await inbox.methods.setMessage('Bye There').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'Bye There');
+    })
 });
